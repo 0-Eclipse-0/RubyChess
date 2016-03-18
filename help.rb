f502051bda2c8da0e5629b82e439cd78696b933f
Eclipse <0-Eclipse-0@users.noreply.github.com>
2016-03-18 15:25:10 +0100

    WORK IN PROGRESS

---
+++ help.rb
@@ -0,0 +1,23 @@
+puts "|----------------------------|"
+puts "|----Chess (Ruby Edition)----|"
+puts "|----------------------------|"
+puts "|-----By: Matt Hambrecht-----|"
+puts "|----------------------------|"
+while fileStat == "open"
+ print "Please enter a command: "
+ usrCommand = gets.chomp
+ if usrCommand == "/help" or "/commands" or "/h" or ".help" or ".commands" or ".h"
+  puts "Commands:
+  puts " H/Help/Commands: Lists commands"
+  puts " Close: Closes help command prompt"
+  puts " Info: Gives user information about the program"
+  puts "Prefix(es):
+  puts " /"
+  puts " ."
+ elsif usrCommand == "/info" or ".info"
+  puts "WIP"
+ elsif usrCommand == "/close" or ".close"
+  puts "Goodbye!"
+  fileStat == "closed"
+ end
+end
